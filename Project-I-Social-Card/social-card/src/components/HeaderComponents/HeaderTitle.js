b05a0539226abd90dfbf32233b6dc556a138679c
Michael Jenkins <mjenkins9605@vidangel.com>
2019-02-27 23:08:28 -0500

    content added, initial layout

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+    return (
+      <div className="headTitle">
+        <h2>Lambda School</h2>
+        <h3>@LambdaSchool • 26 jan</h3>
+      </div>
+    );
+  }
+
+export default HeaderTitle;
